2ae2d489ac58574612b6e9ad7f1faa2469792029
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2022-08-13 11:30:05 +0200

    empty extension

---
+++ src/Special.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MediaWiki\Extension\CocktailSearch;
+class Special extends \SpecialPage
+{
+    function __construct()
+    {
+        parent::__construct('CocktailSearch');
+    }
+
+    function execute( $subPage ) {
+    }
+}
