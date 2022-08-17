37aafd98623c0c92ad3083d1cc388d40b77e7f9b
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2022-08-17 11:28:05 +0200

    don't forget to commit the js
    this is the component that allows for autocomplete of the ingredients

---
+++ modules/ext.CocktailSearch/cocktailsearch.ui.entitysearch.js
@@ -0,0 +1,22 @@
+( function () {
+	'use strict';
+	require( '../../../Wikibase/view/resources/jquery/wikibase/jquery.wikibase.entityselector.js' );
+	$( function () {
+		for (const field of ['#ingredient1', '#ingredient2', '#ingredient3']) {
+			console.log( field );
+			let input = $( field );
+			let hidden = $( field + 'H');
+			console.log( input );
+
+			input
+				.entityselector( {
+					url: mw.config.get( 'wgServer' ) + mw.config.get( 'wgScriptPath' ) + '/api.php',
+					type: 'item',
+					selectOnAutocomplete: true,
+					selected: function( ev, data ) {
+						hidden.val(data);
+					}
+				} );
+		}
+	} );
+}() );
